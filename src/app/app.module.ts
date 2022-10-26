aff2e2143f9f1bfbb8889947ebf70646de0788f0
Yolando <yolandomanullang@gmail.com>
2022-10-26 14:36:42 +0700

    done ep 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { AddCarComponent } from './add-car/add-car.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    AddCarComponent
   ],
   imports: [
     BrowserModule,
